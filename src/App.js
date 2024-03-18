97ca87f1d4f0d7a27cbbad193ac34da4d1bce2a7
manishkumar <manishkumar13may2003@gmail.com>
2024-03-19 03:10:06 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import  "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <User name="Manish" age={21} gmail="manish@gmail.com"/>
+     <User name="Manish" age={21} gmail="manish@gmail.com"/>
     </div>
-  );
-}
+  )
+};
+
+
+const User=(props)=>{
+ return( <div>
+          <h1>{props.name}</h1>
+          <h1>{props.age}</h1>
+          <h1>{props.gmail}</h1>
+          </div> 
+          
+       )
+};
+
 
 export default App;

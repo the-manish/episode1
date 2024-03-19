1512458505dcc1e21421b5ff8916851de9ba503c
manishkumar <manishkumar13may2003@gmail.com>
2024-03-19 19:07:43 +0530

    taversing through conditional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import  "./App.css";
 
 function App() {
+  const planets=[
+    {name:"Mars",isGasPlanet:false},
+    {name:"Earth",isGasPlanet:false},
+    {name:"Jupiter",isGasPlanet:true},
+    {name:"Venus",isGasPlanet:false},
+    {name:"Neptune",isGasPlanet:true},
+    {name:"Uranus",isGasPlanet:true},
+  ];
   
   return (
-    <div className="App">
-     <Jobs salary={9000} position="SDE" company="Google"/>
-     <Jobs salary={100000} position="SDE" company="Microsoft"/>
-     <Jobs salary={1500000} position="SDE" company="Meta"/>
-     
-    </div>
+    <div className="App">{planets.map((planet,key)=>{
+      return(
+        planet.isGasPlanet &&<h1>{planet.name } </h1>
+      )
+    })}</div>
   );
 };
 
 
-const Jobs=(props)=>{
- return( <div>
-          <h1>{props.salary}</h1>
-          <h1>{props.position}</h1>
-          <h1>{props.company}</h1>
-          </div> 
-          
-       );
-};
 
 
 export default App;
